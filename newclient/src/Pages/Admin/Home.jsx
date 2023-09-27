7e826db5d1b05ac1fe230324243d26513eb5a160
arvind2602 <guptaarvind2602@gmail.com>
2023-09-27 15:08:35 +0530

    Added Login and registration pages

---
+++ newclient/src/Pages/Admin/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const adminHome = () => {
+    return (
+        <>
+            <div>Admin Home</div>
+            <a href='/admin/login'>Login</a>
+            <a href='/admin/register'>Register</a>
+            <button>Logout</button>
+        </>
+    )
+}
+
+export default adminHome
