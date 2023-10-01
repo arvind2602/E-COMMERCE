5140493572a39d6b66307966eed023841a364b2f
arvind2602 <guptaarvind2602@gmail.com>
2023-10-02 00:20:02 +0530

    Added get method

--- server/routes/admin/addProduct.js
+++ server/routes/admin/addProduct.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import ProductSchema from '../../models/addProduct.js';
 
-const Product=express.Router();
-Product.post('/addproduct', async (req, res)=>{
+const Product = express.Router();
+Product.post('/products/add', async (req, res) => {
     const { name, price, description, category, quantity, image } = req.body;
     try {
         const newProduct = new ProductSchema({ name, price, description, category, quantity, image });
@@ -10,6 +10,15 @@ Product.post('/addproduct', async (req, res)=>{
         res.status(201).json(newProduct);
     } catch (error) {
         res.status(409).json({ message: error.message });
+    }
+})
+
+Product.get('/products', async (req, res) => {
+    try {
+        const products = await ProductSchema.find();
+        res.status(200).json(products);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
     }
 })
 export default Product;
